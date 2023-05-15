08d52ee96d85fe4ca17486f49d362da6c64ea9a7
Akash Tejani <akash.t@elaunchinfotech.in>
2023-05-15 16:14:58 +0530

    aa

--- config/db.js
+++ config/db.js
@@ -5,11 +5,6 @@ mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Database Conntected Succesfully')
 })
-
-
-
-
-
     .catch((err) => {
         console.log(err)
     })
