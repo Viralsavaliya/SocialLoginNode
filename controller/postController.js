e854639a983519f9291f52c5616c3f0f5856e49e
Akash Tejani <akash.t@elaunchinfotech.in>
2023-05-23 15:08:42 +0530

    aa

--- controller/postController.js
+++ controller/postController.js
@@ -19,22 +19,8 @@ exports.getallpost = async (req, res) => {
     }
 }
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "uploads");
-    },
-    filename: (req, file, cb) => {
-      cb(
-        null,
-        file.fieldname +
-          "-" +
-          Date.now() +
-          "." +
-          file.originalname.split(".").pop()
-      );
-    },
-  });
-  const upload = multer({ storage: storage }).single("file");
+
+
   
   exports.uploadimage = async (req, res) => {
     try {
@@ -59,29 +45,108 @@ const storage = multer.diskStorage({
   };
 
 
+// exports.addpost = async (req, res) => {
+//     try {
+//         let datafilename
+//         const storage = multer.diskStorage({
+//             destination: (req, file, cb) => {
+//               cb(null, "uploads");
+//             },
+//             filename: (req, file, cb) => {
+//               cb(
+//                 null,
+//                 file.fieldname +
+//                   "-" +
+//                   Date.now() +
+//                   "." +
+//                   file.originalname.split(".").pop()
+//               );
+//             },
+//           });
+//           const upload = multer({ storage: storage }).single("image");
+//           upload(req, res, async function (err) {
+//             const fileName = req.file.filename;
+//              datafilename = fileName?.filename });
+
+//         const {id} = req.user;
+//         const {title  , discripation } = req.body;
+//         const newpost = {
+//             title,
+//             image:datafilename,
+//             discripation,
+//             userid:id
+//         }
+//         const post = await Post.create(newpost);
+
+//         res.status(200).json({
+//           success : true,
+//           data : post,
+//           message : "Post get successfully"
+//         });
+//     } catch (error) {
+//         res.status(400).json({
+//             success : false,
+//             message : error.message
+//           });
+        
+//     }
+// }
+
 exports.addpost = async (req, res) => {
     try {
-
-        const {id} = req.user._id;
-        const {title , image , discripation } = req.body;
-        const newpost = {
-            title,
-            image,
-            discripation,
-            userid:id
-        }
-        const post = await Post.create(newpost);
-
-        res.status(200).json({
-          success : true,
-          data : post,
-          message : "Post get successfully"
-        });
-    } catch (error) {
-        res.status(400).json({
-            success : false,
-            message : error.message
+      let datafilename;
+  
+      const storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+          cb(null, "uploads");
+        },
+        filename: (req, file, cb) => {
+          cb(
+            null,
+            file.fieldname + "-" + Date.now() + "." + file.originalname.split(".").pop()
+          );
+        },
+      });
+  
+      const upload = multer({ storage: storage }).single("image");
+  
+      // Middleware to handle the file upload
+      upload(req, res, async function (err) {
+        if (err) {
+          // Handle any upload errors
+          return res.status(400).json({
+            success: false,
+            message: "Error uploading file",
           });
-        
+        }
+  
+        const fileName = req.file.filename;
+        datafilename = fileName;
+  
+        // Proceed with creating the post
+        const { id } = req.user;
+        const { title, discripation } = req.body;
+  
+        const newPost = {
+          title,
+          image: datafilename,
+          discripation,
+          userid: id,
+        };
+  
+        const post = await Post.create(newPost);
+  
+        return res.status(200).json({
+          success: true,
+          data: post,
+          message: "Post created successfully",
+        });
+      });
+    } catch (error) {
+      return res.status(400).json({
+        success: false,
+        message: error.message,
+      });
     }
-}
+  };
+  
